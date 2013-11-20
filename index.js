acd1289559c607fd7d407c119b026d1b298abc9b
Jayy Vis <jayyvis@gmail.com>
2013-11-20 11:16:42 +0530

    simplified handling of sync & async methods

--- index.js
+++ index.js
@@ -13,31 +13,29 @@ module.exports = thunkify;
  * @api public
  */
 
-function thunkify(fn){
-  return function(){
+function thunkify(fn) {
+  return function() {
     var args = [].slice.call(arguments);
     var results;
-    var called;
     var cb;
 
-    args.push(function(){
-      results = arguments;
-
-      if (cb && !called) {
-        called = true;
-        cb.apply(this, results);
+    args.push(function() {
+      if (cb) { //callback is ready. deliver the result.
+        cb.apply(this, arguments);
+      } else { //yet to receive the callback. keep the result.
+        results = arguments;
       }
     });
 
     fn.apply(this, args);
 
-    return function(fn){
-      cb = fn;
-
-      if (results && !called) {
-        called = true;
-        fn.apply(this, results);
+    return function(_cb) {
+      if (results) { //result is ready. deliver it.
+        _cb.apply(this, results);
+      } else { //keep the callback
+        cb = _cb;
       }
-    }
-  }
-};
+    };
+  };
+}
+
